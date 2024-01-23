e54f57830a98c3b6382352dfc97833185da5af93
Jeevitha3105 <jeevigv880@gmail.com>
2024-01-23 20:16:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Banner from './components/Banner';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import About from './components/About';
-import Services from './components/Services';
+import Education from './components/Education'
 import Work from './components/Work';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
@@ -15,10 +16,11 @@ const App = () => {
       <Banner />
       <Nav />
       <About />
-      <Services />
+      <Education />
+      <Skills />
       <Work />
       <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+      
     </div>
   );
 };
